8dabeda5585a907365c56583aae4e26785e30ee4
Suppasit Srisaeng <mumiman@hotmail.com>
2024-12-20 22:16:12 +0700

    Add MST

---
+++ MasterServerToolkit/Bridges/FishNet/BasicRoomsAndLobbies/Scripts/Character/PlayerCharacterProfile.cs
@@ -0,0 +1,31 @@
+﻿#if FISHNET
+using UnityEngine;
+using MasterServerToolkit.MasterServer;
+
+namespace MasterServerToolkit.Bridges.FishNetworking.Character
+{
+    public class PlayerCharacterProfile : PlayerCharacterBehaviour
+    {
+        //converted from mirror example. code was already commented out.
+
+        // protected RoomPlayer roomPlayer;
+
+        // public override void OnStartServer()
+        // {
+        //    base.OnStartServer();
+
+        //    // Get room server player by mirror peer id
+        //    roomPlayer = RoomServer.Instance.GetRoomPlayerByMirrorPeer(netIdentity.connectionToClient);
+
+        //    if(roomPlayer == null)
+        //    {
+        //        Debug.LogError($"Player {netIdentity.connectionToClient.connectionId} could not get its room playerinfo");
+        //        netIdentity.connectionToClient.Disconnect();
+        //        return;
+        //    }
+        // }
+
+        // protected virtual void OnDestroy() { }
+    }
+}
+#endif
